c81c6ee3b6a1d33b0dd919287f3a6bc5f65e2b9e
finma <firmanmaulana2403@gmail.com>
2024-01-25 09:48:37 +0700

    [ADD] success component

---
+++ src/components/SuccessMessage.tsx
@@ -0,0 +1,15 @@
+type SuccessMessageProps = {
+  message: string;
+};
+
+export default function SuccessMessage({ message }: SuccessMessageProps) {
+  if (!message) return null;
+
+  return (
+    <div className="alert alert-success">
+      <div className="flex-1">
+        <label>{message}</label>
+      </div>
+    </div>
+  );
+}
