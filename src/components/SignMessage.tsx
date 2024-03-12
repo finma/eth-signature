e3758729bd65fde1ed64759a770e11347910d099
finma <firmanmaulana2403@gmail.com>
2024-03-12 09:10:29 +0700

    [ADD] input file, hash keccak256, pdf viewer

--- src/components/SignMessage.tsx
+++ src/components/SignMessage.tsx
@@ -3,10 +3,19 @@
 import { useState, useRef } from "react";
 import { ethers } from "ethers";
 import ErrorMessage from "./ErrorMessage";
+import { keccak256 } from "js-sha3";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+
+type Signature =
+  | {
+      message: string;
+      signature: string;
+      address: string;
+    }
+  | undefined;
 
 const signMessage = async ({ setError, message }: any) => {
   try {
-    console.log({ message });
     if (!window.ethereum)
       throw new Error("No crypto wallet found. Please install it.");
 
@@ -32,17 +41,38 @@ const signMessage = async ({ setError, message }: any) => {
 
 export default function SignMessage() {
   const resultBox = useRef();
-  const [signatures, setSignatures] = useState<any>([]);
-  const [error, setError] = useState<any>();
+  const [signatures, setSignatures] = useState<Signature[]>([]);
+  const [error, setError] = useState<string>();
+  const [selectedDocs, setSelectedDocs] = useState<File[]>([]);
+  const [messageHash, setMessageHash] = useState<string>();
+
+  const handleInput = (e: React.ChangeEvent<any>) => {
+    e.target.files?.length && setSelectedDocs(Array.from(e.target.files));
+
+    if (e.target.files?.length) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const arrayBuffer = reader.result!;
+        const hash = keccak256(arrayBuffer);
+        setMessageHash(hash);
+        // console.log("HASH: ", hash);
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  };
 
   const handleSign = async (e: React.ChangeEvent<any>) => {
     e.preventDefault();
-    if (e.target !== null) {
-      const data = new FormData(e.target);
+    if (selectedDocs.length) {
+      // const data = new FormData(e.target);
       setError("");
-      const sig = await signMessage({
+      const sig: Signature = await signMessage({
         setError,
-        message: data.get("message")?.toString(),
+        // message: data.get("message")?.toString(),
+        message: messageHash,
       });
       if (sig) {
         setSignatures([...signatures, sig]);
@@ -59,16 +89,39 @@ export default function SignMessage() {
           </h1>
           <div className="">
             <div className="my-3">
-              <textarea
-                required
-                // type="text"
-                name="message"
-                className="textarea w-full h-24 textarea-bordered focus:ring focus:outline-none"
-                placeholder="Message"
-              />
+              {/* MESSAGE WITH TEXT */}
+              {/* <textarea
+                  required
+                  // type="text"
+                  name="message"
+                  className="textarea w-full h-24 textarea-bordered focus:ring focus:outline-none"
+                  placeholder="Message"
+                /> */}
+
+              {/* MESSAGE WITH FILE */}
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text font-medium">Pilih dokumen</span>
+                </div>
+                <input
+                  type="file"
+                  accept=".pdf"
+                  onChange={handleInput}
+                  className="file-input file-input-bordered file-input-primary file-input-sm w-full text-gray-400 text-sm"
+                />
+              </label>
             </div>
           </div>
         </main>
+
+        {/* PDF VIEWER */}
+        <DocViewer
+          documents={selectedDocs.map((file) => ({
+            uri: window.URL.createObjectURL(file),
+            fileName: file.name,
+          }))}
+          pluginRenderers={DocViewerRenderers}
+        />
         <footer className="p-4">
           <button
             type="submit"
@@ -78,6 +131,7 @@ export default function SignMessage() {
           </button>
           <ErrorMessage message={error} />
         </footer>
+
         {signatures.map((sig: any, idx: number) => {
           return (
             <div className="p-2" key={sig}>
