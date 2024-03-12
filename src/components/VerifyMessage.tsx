f04d73de379e637afe316ca2b8ba36cfa2a101f7
finma <firmanmaulana2403@gmail.com>
2024-03-12 09:10:34 +0700

    [ADD] input file, hash keccak256, pdf viewer

--- src/components/VerifyMessage.tsx
+++ src/components/VerifyMessage.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { ethers } from "ethers";
 import ErrorMessage from "./ErrorMessage";
 import SuccessMessage from "./SuccessMessage";
+import { keccak256 } from "js-sha3";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 const verifyMessage = async ({ message, address, signature }: any) => {
   try {
-    const signerAddr = await ethers.utils.verifyMessage(message, signature);
+    const signerAddr = ethers.utils.verifyMessage(message, signature);
     if (signerAddr !== address) {
       return false;
     }
@@ -20,8 +22,28 @@ const verifyMessage = async ({ message, address, signature }: any) => {
 };
 
 export default function VerifyMessage() {
-  const [error, setError] = useState<any>();
-  const [successMsg, setSuccessMsg] = useState<any>();
+  const [error, setError] = useState<string>();
+  const [successMsg, setSuccessMsg] = useState<string>();
+  const [selectedDocs, setSelectedDocs] = useState<File[]>([]);
+  const [messageHash, setMessageHash] = useState<string>();
+
+  const handleInput = (e: React.ChangeEvent<any>) => {
+    e.target.files?.length && setSelectedDocs(Array.from(e.target.files));
+
+    if (e.target.files?.length) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const arrayBuffer = reader.result!;
+        const hash = keccak256(arrayBuffer);
+        setMessageHash(hash);
+        // console.log("HASH", hash);
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  };
 
   const handleVerification = async (e: React.ChangeEvent<any>) => {
     e.preventDefault();
@@ -30,7 +52,8 @@ export default function VerifyMessage() {
     setError("");
     const isValid = await verifyMessage({
       setError,
-      message: data.get("message"),
+      // message: data.get("message"),
+      message: messageHash,
       address: data.get("address"),
       signature: data.get("signature"),
     });
@@ -51,18 +74,31 @@ export default function VerifyMessage() {
           </h1>
           <div className="">
             <div className="my-3">
-              <textarea
+              {/* MESSAGE WITH TEXT */}
+              {/* <textarea
                 required
                 // type="text"
                 name="message"
                 className="textarea w-full h-24 textarea-bordered focus:ring focus:outline-none"
                 placeholder="Message"
-              />
+              /> */}
+
+              {/* MESSAGE WITH FILE */}
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text font-medium">Pilih dokumen</span>
+                </div>
+                <input
+                  type="file"
+                  accept=".pdf"
+                  onChange={handleInput}
+                  className="file-input file-input-bordered file-input-primary file-input-sm w-full text-gray-400 text-sm"
+                />
+              </label>
             </div>
             <div className="my-3">
               <textarea
                 required
-                // type="text"
                 name="signature"
                 className="textarea w-full h-24 textarea-bordered focus:ring focus:outline-none"
                 placeholder="Signature"
@@ -79,6 +115,15 @@ export default function VerifyMessage() {
             </div>
           </div>
         </main>
+
+        {/* PDF VIEWER */}
+        <DocViewer
+          documents={selectedDocs.map((file) => ({
+            uri: window.URL.createObjectURL(file),
+            fileName: file.name,
+          }))}
+          pluginRenderers={DocViewerRenderers}
+        />
         <footer className="p-4">
           <button
             type="submit"
