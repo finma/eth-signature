d83c68e2971d35a8ed59934465581797fbae9da4
finma <firmanmaulana2403@gmail.com>
2024-03-14 11:09:58 +0700

    [ADD] next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, os: false, path: false };
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
 
 export default nextConfig;
