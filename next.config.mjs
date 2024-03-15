df8922809affea01e8fff82bbe6a4a7e347ff833
finma <firmanmaulana2403@gmail.com>
2024-03-15 22:00:04 +0700

    [FIX] next config error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack5: true,
+  // webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, os: false, path: false };
     config.resolve.alias.canvas = false;
