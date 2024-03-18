6bf3b45dbc3a95d75902e09b6b2fa6f4ba4a529f
finma <firmanmaulana2403@gmail.com>
2024-03-18 10:17:21 +0700

    [ADD] firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJg8NUvnTWikG-MSHkrp-3MZ619rOh9Ok",
+  authDomain: "eth-signature.firebaseapp.com",
+  projectId: "eth-signature",
+  storageBucket: "eth-signature.appspot.com",
+  messagingSenderId: "838275412498",
+  appId: "1:838275412498:web:08f704288a60b8f230decb",
+  measurementId: "G-CR04NXTP42",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
