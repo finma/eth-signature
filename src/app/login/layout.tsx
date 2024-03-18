b6177aa8c1fab1c37efedd6bbdea7b42341e1f57
finma <firmanmaulana2403@gmail.com>
2024-03-18 21:44:10 +0700

    [CHG] remove metadata & fix layout

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,19 +1,7 @@
-import type { Metadata } from "next";
-import "./../globals.css";
-
-export const metadata: Metadata = {
-  title: "ETH Signature",
-  description: "Website Digital Signature with Blockchain",
-};
-
-export default function RootLayout({
+export default function LoginLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" data-theme="light">
-      <body>{children}</body>
-    </html>
-  );
+  return <main>{children}</main>;
 }
