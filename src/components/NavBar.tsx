155159f2a225d7717e465a9fd9f58361b2122962
finma <firmanmaulana2403@gmail.com>
2024-03-18 21:44:36 +0700

    [ADD] handle signout & add signin button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,46 +1,79 @@
-/* eslint-disable @next/next/no-img-element */
-import React from "react";
+"use client";
+
+import { UserAuth } from "@/context/AuthContext";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const NavBar = () => {
+  const { logOut, user } = UserAuth();
+  const [currentUser, setCurrentUser] = useState(user);
+
+  useEffect(() => {
+    // console.log("user: ", user);
+    setCurrentUser(user);
+  }, [user]);
+
+  const handleLogout = async () => {
+    const result = await logOut();
+
+    if (result.error) {
+      toast.error(result.message);
+    } else {
+      toast.success(result.message);
+      setCurrentUser({});
+    }
+  };
+
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          {Object.keys(currentUser).length !== 0 ? (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full text-3xl">
+                  <FontAwesomeIcon icon={faUserCircle} />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <button>History</button>
+                </li>
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </ul>
             </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
+          ) : (
+            <Link href="/login" className="btn btn-primary btn-sm text-white ">
+              SignIn
+            </Link>
+          )}
         </div>
       </div>
-    </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="light"
+      />
+    </>
   );
 };
 
