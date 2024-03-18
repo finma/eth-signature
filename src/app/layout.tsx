4fa4aa09d2c5909ace5a4a3839f52d3e8921d261
finma <firmanmaulana2403@gmail.com>
2024-03-18 16:55:21 +0700

    [CHG] remove metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
-import type { Metadata } from "next";
+"use client";
+
+// import type { Metadata } from "next";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthContextProvider } from "@/context/AuthContext";
 
-export const metadata: Metadata = {
-  title: "ETH Signature",
-  description: "Website Digital Signature with Blockchain",
-};
+// export const metadata: Metadata = {
+//   title: "ETH Signature",
+//   description: "Website Digital Signature with Blockchain",
+// };
 
 export default function RootLayout({
   children,
