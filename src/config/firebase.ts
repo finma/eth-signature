25779ce30578bbed65f62b427650a3e79644737a
finma <firmanmaulana2403@gmail.com>
2024-03-19 21:37:21 +0700

    [ADD] firebase storage & firestore database

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAJg8NUvnTWikG-MSHkrp-3MZ619rOh9Ok",
@@ -14,5 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth };
+export { auth, db, storage };
