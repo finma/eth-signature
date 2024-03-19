79e5093244062fac19027d0fb9ee9b632c57621a
finma <firmanmaulana2403@gmail.com>
2024-03-19 16:10:22 +0700

    [ADD] check if user not login

--- src/app/history/layout.tsx
+++ src/app/history/layout.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { UserAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function HistoryLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+  const { user } = UserAuth();
+
+  useEffect(() => {
+    if (Object.keys(user).length === 0) {
+      router.push("/login");
+    }
+  }, [router, user]);
+
   return <main className="min-h-screen bg-base-200">{children}</main>;
 }
