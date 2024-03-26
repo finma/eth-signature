e5f599551d2c6fa0b3ca57aea46f02e520e9c5f0
finma <firmanmaulana2403@gmail.com>
2024-03-26 11:06:13 +0700

    [CHG] update title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -33,7 +33,7 @@ export default function NavBar() {
       <div className="navbar bg-base-100">
         <div className="flex-1">
           <Link href="/" className="btn btn-ghost text-xl">
-            Versidig
+            SignDocu
           </Link>
         </div>
         <div className="flex-none">
